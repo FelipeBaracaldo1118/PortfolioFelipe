ee3698eae51c5c8a38a59cfbb8ff81934e2c706f
FelipeBaracaldo1118 <127355419+FelipeBaracaldo1118@users.noreply.github.com>
2025-01-28 11:44:29 -0500

    inicializando proyecto
    Se inicializa proyecto, ya avanzado con informacion y estilo inicial.

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function openToWork(){
+    location.href="https://www.linkedin.com/in/sebastian-felipe-baracaldo-amado-0a062b181/"
+}
+function CVdownload(){
+    const url ="./assets/documents/CV SEBASTIAN F BARACALDO.PDF"
+    window.open(url, "_blank")
+}
